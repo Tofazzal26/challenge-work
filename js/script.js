6e8411cf3e567a08b994614b74943b13ccdbd59b
Tofazzal26 <afranislamabir6789@gmail.com>
2023-12-15 00:35:38 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,140 @@
+
+
+
+
+
+
+const changeBtn = document.getElementById("Change");
+
+
+changeBtn.addEventListener("click", function () {
+
+    const workContent = document.getElementById("work-container");
+
+    workContent.style.display = "none";
+
+    const cssCome = document.getElementById("Css-Hide");
+    cssCome.style.display = "block";
+
+
+    const htmlHide = document.getElementById("Html-Hide")
+    htmlHide.style.display = "none"
+
+    const javaHide = document.getElementById("Java-Hide")
+    javaHide.style.display = "none";
+
+    const codingoverFlow = document.getElementById("Codigniter");
+    codingoverFlow.style.display = "none";
+})
+
+
+
+
+const changeCss = document.getElementById("Change-2");
+
+changeCss.addEventListener("click", function () {
+    
+    const cssCome = document.getElementById("Css-Hide");
+
+    cssCome.style.display = "none"
+
+    const htmlBlock = document.getElementById("Html-Hide")
+    htmlBlock.style.display = "block";
+
+    const workContent = document.getElementById("work-container");
+    workContent.style.display = "none"
+
+    const javaHiding = document.getElementById("Java-Hide")
+    javaHiding.style.display = "none";
+
+    const laraNai = document.getElementById("Laravel-Hide");
+    laraNai.style.display = "none";
+
+    const codinghidden = document.getElementById("Codigniter")
+    codinghidden.style.display = "none";
+
+})
+
+
+const javaChang = document.getElementById("Java")
+
+javaChang.addEventListener("click", function () {
+    
+    const javaIn = document.getElementById("Java-Hide");
+    javaIn.style.display = "block"
+
+    const cssBlock = document.getElementById("Css-Hide")
+    cssBlock.style.display = "none";
+
+    const htmlBlock = document.getElementById("Html-Hide")
+    htmlBlock.style.display = "none";
+
+    const afterProject = document.getElementById("work-container")
+    afterProject.style.display = "none";
+
+    const laraBlock = document.getElementById("Laravel-Hide")
+    laraBlock.style.display = "none";
+
+    const javaBlocking = document.getElementById("Codigniter")
+    javaBlocking.style.display = "none";
+})
+
+
+
+
+const laravelIN = document.getElementById("lara");
+
+laravelIN.addEventListener("click", () => {
+
+    const laraIn = document.getElementById("Laravel-Hide");
+    laraIn.style.display = "block"
+
+    const mainpage = document.getElementById("work-container");
+    mainpage.style.display = "none";
+
+    const htmlHiding = document.getElementById("Css-Hide")
+    htmlHiding.style.display = "none";
+
+    const cssLara = document.getElementById("Html-Hide")
+    cssLara.style.display = "none";
+
+    const javaLara = document.getElementById("Java-Hide")
+    javaLara.style.display = "none";
+
+    const codingHid = document.getElementById("Codigniter");
+    codingHid.style.display = "none";
+
+})
+
+
+
+
+const codingBlog = document.getElementById("Codings");
+
+codingBlog.addEventListener("click", () => {
+    const codingIn = document.getElementById("Codigniter")
+    codingIn.style.display = "block"
+
+    const afterCoding = document.getElementById("work-container")
+    afterCoding.style.display = "none"
+
+    const htmlhiddin = document.getElementById("Css-Hide");
+    htmlhiddin.style.display = "none"
+
+    const cssHidden = document.getElementById("Html-Hide");
+    cssHidden.style.display = "none";
+
+    const javaHidden = document.getElementById("Java-Hide")
+    javaHidden.style.display = "none";
+
+    const laraHidden = document.getElementById("Laravel-Hide")
+    laraHidden.style.display = "none";
+})
+
+
+
+
+
+
+
+
